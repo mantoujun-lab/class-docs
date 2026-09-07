449ca0ee9a16a34241fc77e1d64ed882c47fe1c9
mantoujun12 <202384594+mantoujun12@users.noreply.github.com>
2026-09-07 15:28:41 +0800

    feat: add waline

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,8 +1,15 @@
 /// <reference types="vitepress/client" />
 
+import { h } from 'vue'
 import DefaultTheme from 'vitepress/theme'
 import './custom.css'
+import WalineComment from './components/WalineComment.vue'
 
 export default {
   extends: DefaultTheme,
+  Layout() {
+    return h(DefaultTheme.Layout, null, {
+      'doc-bottom': () => h(WalineComment),
+    })
+  },
 }
